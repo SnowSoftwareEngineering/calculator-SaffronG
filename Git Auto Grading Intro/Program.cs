8d2a9bbba2e43dc8a7d098b057cdd6a199cf63a8
SaffronGamer <saffrongamer167@gmail.com>
2024-09-17 18:27:58 -0600

    Completed the methods and implemented them, printing the values

--- Git Auto Grading Intro/Program.cs
+++ Git Auto Grading Intro/Program.cs
@@ -7,30 +7,32 @@
             // Welcome to the calculator project, complete the methods
 
             // Call the methods here ex.
-            int result = Add(2, 3);
-            Console.WriteLine("The result of adding 2 and 3 is: " + result);
+            Console.WriteLine("The result of adding 2 and 3 is: " + Add(2,3));
+            Console.WriteLine("The result of subtracting 2 and 3 is: " + Subtract(2,3));
+            Console.WriteLine("The result of multiplying 2 and 3 is: " + Multiply(2,3));
+            Console.WriteLine("The result of dividing 2 and 3 is: " + Divide(2,3));
         }
 
         // Methods Here
         public static int Add(int a, int b)
         {
             // Code
-            return 0; // Placeholder
+            return a + b; // Placeholder
         }
         public static int Subtract(int a, int b)
         {
             // Code
-            return 0; // Placeholder
+            return a - b; // Placeholder
         }
         public static int Multiply(int a, int b)
         {
             // Code
-            return 0; // Placeholder
+            return a * b; // Placeholder
         }
         public static decimal Divide(int a, int b)
         {
             // Code
-            return 0; // Placeholder
+            return (decimal) a / b; // Placeholder
         }
     }
 }
